956b1c4081c2d8b5257f7a356ef33c7563970031
Carlos Henrique <carlos@vendedoor.com.br>
2018-12-21 15:43:57 -0200

    removed dependencies 'collect.js and luxon.js'

--- test/DateTime.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { DateTime } from '../src/index';
-
-describe('DateTime', () => {
-  it('instance of', () => {
-    expect(DateTime.local()).to.be.an.instanceof(DateTime);
-  });
-});
